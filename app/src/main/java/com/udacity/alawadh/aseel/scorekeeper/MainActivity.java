4cb110e09df3f121c1a493b35bc6e762f4405cba
Aseel AlAwadh <aseel.alawadh@gmail.com>
2018-03-28 20:35:42 +0300

    doing the change on textView & buttons based on review comments.

--- app/src/main/java/com/udacity/alawadh/aseel/scorekeeper/MainActivity.java
+++ app/src/main/java/com/udacity/alawadh/aseel/scorekeeper/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
 
     int scoreMadrid = 0;
     int scoreBarcelona = 0;
+    int foulMadrid = 0;
+    int foulBarcelona = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +22,27 @@ public class MainActivity extends AppCompatActivity {
         scoreMadrid = scoreMadrid + 1;
          displayForMadrid(scoreMadrid);
     }
+    public void addFoulForTeamMadrid(View v) {
+        foulMadrid = foulMadrid + 1;
+        displayFoulForMadrid(foulMadrid);
+
+    }
 
     public void addOneForTeamBarcelona(View v) {
         scoreBarcelona = scoreBarcelona + 1;
         displayForBarcelona(scoreBarcelona);
     }
 
+    public void addFoulForTeamBarcelona(View v) {
+        foulBarcelona = foulBarcelona+ 1;
+        displayFoulForBarcelona(foulBarcelona);
+    }
+
     public void Rest(View v) {
         displayForMadrid(0);
+        displayFoulForMadrid(0);
         displayForBarcelona(0);
+        displayFoulForBarcelona(0);
     }
 
     public void displayForMadrid(int score) {
@@ -36,8 +50,18 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
+    public void displayFoulForMadrid(int score) {
+        TextView scoreView = findViewById(R.id.foul_madrid);
+        scoreView.setText(String.valueOf(score));
+    }
+
     public void displayForBarcelona(int score) {
         TextView scoreView = findViewById(R.id.score_barcelona);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayFoulForBarcelona(int score) {
+        TextView scoreView = findViewById(R.id.foul_barcelona);
         scoreView.setText(String.valueOf(score));
     }
 
